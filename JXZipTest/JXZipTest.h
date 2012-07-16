5816774be5a8c33a13240c1d455aa8fab483e447
Jan <github@geheimwerk.de>
2012-07-16 10:04:26 +0200

    Adding empty JXZip unit tests.

---
+++ JXZipTest/JXZipTest.h
@@ -0,0 +1,13 @@
+//
+//  JXZipTest.h
+//  JXZipTest
+//
+//  Created by Jan on 16.07.12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface JXZipTest : SenTestCase
+
+@end
---
+++ JXZipTest/JXZipTest.m
@@ -0,0 +1,32 @@
+//
+//  JXZipTest.m
+//  JXZipTest
+//
+//  Created by Jan on 16.07.12.
+//
+//
+
+#import "JXZipTest.h"
+
+@implementation JXZipTest
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in JXZipTest");
+}
+
+@end
